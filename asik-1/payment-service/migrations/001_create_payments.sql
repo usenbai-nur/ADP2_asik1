87fff7427ac49a441637625fd0c9cfb296852638
assignment <usenbainurdaulet1@gmail.com>
2026-04-12 10:49:29 +0500

    updating migrations of payment

--- asik-1/payment-service/migrations/001_create_payments.sql
+++ asik-1/payment-service/migrations/001_create_payments.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS payments (
-    id             TEXT        PRIMARY KEY,
-    order_id       TEXT        NOT NULL,
-    transaction_id TEXT        NOT NULL UNIQUE,
-    amount         BIGINT      NOT NULL CHECK (amount > 0),   -- stored in cents, never float
-    status         TEXT        NOT NULL,                       -- Authorized | Declined
-    created_at     TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    id              TEXT        PRIMARY KEY,
+    order_id        TEXT        NOT NULL,
+    transaction_id  TEXT        NOT NULL UNIQUE,
+    amount          BIGINT      NOT NULL CHECK (amount > 0),
+    status          TEXT        NOT NULL,
+    created_at      TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX IF NOT EXISTS idx_payments_order_id ON payments(order_id);
-CREATE INDEX IF NOT EXISTS idx_payments_status   ON payments(status);
+CREATE INDEX IF NOT EXISTS idx_payments_status ON payments(status);
